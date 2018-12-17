f5b7e935dff37467f2d946d98eb1c6489a8c33f0
Noel Mansour <327631+noelmansour@users.noreply.github.com>
2018-12-17 17:02:46 -0500

    Set example applink and add event sink for iOS

--- ios/Classes/LinkHandlerPlugin.m
+++ ios/Classes/LinkHandlerPlugin.m
@@ -1,20 +1,42 @@
 #import "LinkHandlerPlugin.h"
 
-@implementation LinkHandlerPlugin
+@interface LinkHandlerPlugin () <FlutterStreamHandler>
+@end
+
+@implementation LinkHandlerPlugin  {
+    FlutterEventSink _eventSink;
+}
+
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"plugins.snowble.com/link_handler"
-            binaryMessenger:[registrar messenger]];
-  LinkHandlerPlugin* instance = [[LinkHandlerPlugin alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+                                     methodChannelWithName:@"plugins.snowble.com/link_handler"
+                                     binaryMessenger:[registrar messenger]];
+    LinkHandlerPlugin* instance = [[LinkHandlerPlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
+
+    FlutterEventChannel* streamChannel =
+    [FlutterEventChannel eventChannelWithName:@"plugins.snowble.com/links"
+                              binaryMessenger:[registrar messenger]];
+    [streamChannel setStreamHandler:instance];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getLastLink" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
+    if ([@"getLastLink" isEqualToString:call.method]) {
+        result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+    } else {
+        result(FlutterMethodNotImplemented);
+    }
+}
+
+#pragma mark FlutterStreamHandler impl
+
+- (FlutterError*)onListenWithArguments:(id)arguments eventSink:(FlutterEventSink)eventSink {
+    _eventSink = eventSink;
+    return nil;
+}
+
+- (FlutterError*)onCancelWithArguments:(id)arguments {
+    return nil;
 }
 
 @end
